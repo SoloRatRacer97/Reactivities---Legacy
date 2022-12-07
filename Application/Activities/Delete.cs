cb954e7eea5ba37f349331c315a0a596b9adade4
SoloRatRacer97 <todd.j.anderson.1997@gmail.com>
2022-12-06 16:14:24 -0800

    End of section 4

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -20,7 +20,6 @@ namespace Application.Activities
             {
                 _context = context;
             }
-
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
